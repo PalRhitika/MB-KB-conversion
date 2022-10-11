db49f0071fde60e395b4b09afc6da252a1792136
PalRhitika <palrhiti52@gmail.com>
2022-10-11 21:16:59 +0545

    Assignment of MB,KB conversion

---
+++ assignment.js
@@ -0,0 +1,7 @@
+function conversion(a){
+    console.log(a+"MB is equal to "+a*1024+"KB");
+
+
+}
+
+module.exports.mbconversion=conversion;
---
+++ test.js
@@ -0,0 +1,8 @@
+//  var multiplyresult=require('./multiply'); // yo chai module ma tanera store gareko .
+
+//  multiplyresult.multiply123(10,20);// yo chai module call gareko 
+
+ var conversionresult=require('./assignment');
+
+ conversionresult.mbconversion(2);
+
